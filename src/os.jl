fa46b6ae4e1c6cb7ffdb447847163a0e61f3bf5f
charlieIT <>
2022-03-15 20:15:58 +0000

    Fixed structures to accomodate some missing fields when parsing xml to structs Added fields definition for missing fields Added accessor methods Redoredered props

--- src/os.jl
+++ src/os.jl
@@ -1,38 +1,46 @@
 Base.@kwdef mutable struct PortUsed <:Leaf
-    state::String   = ""
-    proto::String   = ""
     id::Int         = 0
+    proto::String   = ""
+    state::String   = ""
 end
 Marshalling.fields(::Type{PortUsed}) = (portid=:id,)
 
 Base.@kwdef mutable struct OSFingerprint <:Leaf
     fingerprint::String = ""
 end
+fingerprint(osf::OSFingerprint) = osf.fingerprint
 
 Base.@kwdef mutable struct OSClass <:Marsh
-    type::String            = ""
-    vendor::String          = ""
+    accuracy::Int           = 0
     family::String          = ""
     gen::String             = ""
-    accuracy::Int           = 0
+    type::String            = ""
+    vendor::String          = ""
     cpes::Vector{String}    = Vector{String}()
 end
-family(class::OSClass) = class.family
-Marshalling.fields(::Type{OSClass}) = (
-    cpe = :cpes,
-    osfamily = :family,
-    osgen = :gen
-)
+Marshalling.fields(::Type{OSClass}) = (cpe = :cpes, osfamily = :family, osgen = :gen)
+
+accuracy(class::OSClass)    = class.accuracy
+family(class::OSClass)      = class.family
+gen(class::OSClass)         = class.gen
+vendor(class::OSClass)      = class.vendor
+type(class::OSClass)        = class.type
+
 
 Base.@kwdef mutable struct OSMatch <:Marsh
-    name::String                = ""
     accuracy::Int               = 0
     line::Int                   = 0
+    name::String                = ""
     classes::Vector{OSClass}    = Vector{OSClass}()
 end
-Marshalling.fields(::Type{OSMatch}) = (
-    osclass = :classes,
-)
+Marshalling.fields(::Type{OSMatch}) = (osclass = :classes,)
+
+accuracy(match::OSMatch)    = match.accuracy
+line(match::OSMatch)        = match.line
+name(match::OSMatch)        = match.name
+
+classes(match::OSMatch)     = match.classes
+Base.string(match::OSMatch) = name(match)
 
 Base.@kwdef mutable struct OS <:Marsh
     ports::Vector{PortUsed}              = Vector{PortUsed}()
