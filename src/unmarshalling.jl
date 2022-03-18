88847ed519da450644d1f000973168b2177af145
charlieIT <>
2022-03-18 19:48:47 +0000

    updated xml to structure logic updated serialization methods

--- src/unmarshalling.jl
+++ src/unmarshalling.jl
@@ -1,6 +1,7 @@
 module Marshalling
 
 using JSON
+using StructTypes
 using XMLDict
 
 export Leaf, Marsh
@@ -10,7 +11,9 @@ export fields, unmarshall
 """
     Marsh
 
-XML nodes that either have children or whose associated Type is comprised of other `Marsh` types
+XML nodes to be mapped to a structure
+
+Usually types that either have children or whose associated Type is comprised of other `Marsh` types
 """
 abstract type Marsh end
 
@@ -24,34 +27,49 @@ When unmarshalling, either parsed to a Type or attributes are mapped to Type pro
 abstract type Leaf <: Marsh end
 
 """
-    fields(::Type{T}) :: NamedTuple
-
-Defines assocation between xml attrs and properties of T
-"""
-fields(::Type{T}) where T = ()
-
-"""
     getfield(::Type{T}, k)
 
-Obtain property `k` as defined under `fields(::T)`, or default to `k`
+Wrapper around StructTypes.julianame(tuple::Tuple, serializationname::Symbol)
+
+Obtain property `k` of `T`, as defined under `names(::T)`, or default to `k`
 """
 function getfield(::Type{T}, k::Symbol) where T<:Marsh
-    mappedfields = fields(T)
-    return isempty(mappedfields) || !haskey(mappedfields, k) ? k : mappedfields[k]
+    return StructTypes.julianame(StructTypes.names(T), k)
 end
 getfield(::Type{T}, k::String) where T<:Marsh = getfield(T, Symbol(k))
 
+"""
+    Parse(::Type{T}, xml) where T<:Union{Marsh, Vector{<:Marsh}}
+
+Apply some parsing logic or transformation to input `xml` as a whole prior to creating an instanc of `T`
+"""
 function Parse(::Type{T}, xml) where T<:Union{Marsh, Vector{<:Marsh}} return xml end
 
-unmarshall(::Type{T}, v) where T<:Any = v
+"""
+    Parse(::Type{T}, prop::Val{Symbol}, v)
 
-unmarshall(::Type{I}, s::String) where I<:Int = parse(I, s)
-unmarshall(::Type{V}, s::S) where {V<:Vector, S<:AbstractString} = [string(s)]
-function unmarshall(::Type{T}, xml::XMLDict.XMLDictElement) where T<:Marsh
+Apply some transformation to value `v` of a specific `prop` of T
+"""
+function Parse(::Type{T}, prop::V, v) where {T<:Union{Marsh, Vector{<:Marsh}} , V<:Val}
+    return v
+end
+
+StructTypes.construct(::Type{T}, v) where T<:Any = v
+StructTypes.construct(::Type{I}, s::String) where I<:Int = parse(I, s)
+StructTypes.construct(::Type{V}, s::S) where {V<:Vector, S<:AbstractString} = [string(s)]
+
+function StructTypes.construct(::Type{Vector{T}}, xml::Vector) where T<:Marsh
+    return [StructTypes.construct(T, entry) for entry in xml]
+end
+function StructTypes.construct(::Type{Vector{T}}, xml::XMLDict.XMLDictElement) where T<:Marsh
+    return Vector{T}([StructTypes.construct(T, xml)])
+end
+
+function StructTypes.construct(::Type{T}, xml::XMLDict.XMLDictElement) where T<:Marsh
     this = T()
 
     for (k, v) in Parse(T, xml)
-        field = getfield(T, k)
+        field    = getfield(T, k)
         typemap  = Dict([name=>type for (name, type) in zip(fieldnames(T), T.types)])
 
         if field in fieldnames(T)
@@ -65,7 +83,7 @@ function unmarshall(::Type{T}, xml::XMLDict.XMLDictElement) where T<:Marsh
                         end
                     else
                         try
-                            v = unmarshall(proptype, v)
+                            v = StructTypes.construct(proptype, v)
                         catch err
                             v = Base.convert(proptype, v)
                         end
@@ -74,21 +92,22 @@ function unmarshall(::Type{T}, xml::XMLDict.XMLDictElement) where T<:Marsh
                 Base.setproperty!(this, field, v)
             else
                 if !isempty(v)
-                    Base.setproperty!(this, field, unmarshall(proptype, v))
+                    Base.setproperty!(this, field, StructTypes.construct(proptype, v))
                 end
             end
         else
-            return unmarshall(T, v)
+            return StructTypes.construct(T, v)
         end
     end
     return this
 end
-function unmarshall(::Type{Vector{T}}, xml::Vector) where T<:Marsh
-    return [unmarshall(T, entry) for entry in xml]
-end
-function unmarshall(::Type{Vector{T}}, xml::XMLDict.XMLDictElement) where T<:Marsh
-    return Vector{T}([unmarshall(T, xml)])
-end
+
+"""
+    unmarshall(::Type{T}, xml)
+
+Dispatcher to prevent some inconvient overwrites when defining StructTypes.construct(T, input) over Base, Core or shared Types from other libs
+"""
+function unmarshall(::Type{T}, xml) where T return StructTypes.construct(T, xml) end
 
 isleaf(value) = !(value isa XMLDict.XMLDictElement)
 function isleaf(k::T, v) where T<:Union{Symbol, String}
@@ -107,9 +126,9 @@ function Base.Dict(m::T; replace=false) where T<:Marsh
             value = Dict(value)
         end
         if replace
-            iter = collect(zip(keys(fields(T)), values(fields(T))))
-            if (idx = findall(x->last(x) == prop, iter)) |> !isempty
-                prop = first(first(iter[idx]))
+            names = StructTypes.names(T)
+            if (idx = findall(x->first(x) == prop, names)) |> !isempty
+                prop = last(first(names[idx]))
             end
         end
         out[string(prop)] = value
