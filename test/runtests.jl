c436367a8b0ba67181c4b6928b52c8b3dc1401a4
charlieIT <>
2022-03-18 19:45:58 +0000

    initial test setup with stub tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,38 @@
-#=
-
-s = NMAP.Scanner(
-           NMAP.ports("80", "22"),
-           "-O",
-           "-sV",
-           NMAP.targets("127.0.0.1"))
-
-s = NMAP.Scanner(
-           NMAP.ports("80", "22"),
-           "-O",
-           "-sV",
-           NMAP.targets("127.0.0.1", "192.168.80.2"))
-
-=#
+using Dates
+using JSON
+using NMAP
 using Test
-@test 1==1
+using XMLDict
+
+@testset "Bulk Options" begin
+    scanner = NMAP.Scanner()
+    for name in names(NMAP, all=true, imported=false)
+        f = getfield(NMAP, name)
+        if f isa Function && NMAP.Option in Base.return_types(f)
+            ms = methods(f).ms
+            for method in ms
+                test_scanner = NMAP.Scanner()
+                if method.nargs < 2
+                    opt = f()
+                    test_scanner(opt)
+                    len_diff = length(test_scanner.args)
+                    cur_len  = length(scanner.args)
+                    scanner(opt)
+                    @test cur_len + len_diff == length(scanner.args)
+                end
+            end
+        end
+    end
+end
+
+@testset "XML to Scan" begin
+    for (root, dir, files) in walkdir(joinpath(@__DIR__, "resources"))
+        for file in files
+            input = joinpath(root, file)
+            scan  = NMAP.Scan(read(input))
+            @test scan isa Scan
+            xml   = XMLDict.parse_xml(String(read(joinpath(root, file))))
+            @test xml[:args] == scan.args
+        end
+    end
+end
