c38c22b84589169933e8b7a1eb23b8f5aa61b2df
charlieIT <>
2022-03-18 19:39:36 +0000

    Added StructTypes Updated docstrings

--- src/NMAP.jl
+++ src/NMAP.jl
@@ -1,7 +1,35 @@
 """
-NMAP library aims to provide idiomatic nmap bindings, in order to aid the creation of security audit tools using Julia
+NMAP library aims to provide idiomatic nmap bindings to aid the creation of security audit tools using Julia
+
+## Scanner
+
+## Scan
+
+## run!
+
+## Examples
+
+Create scanners
+```julia
+scanner = NMAP.Scanner(
+    NMAP.targets("localhost", "127.0.0.1", "scanme.nmap.org"),
+    NMAP.ports("21-25", "80", "443", "8080"),
+    NMAP.service_info(),
+    NMAP.os_detection(),
+    NMAP.default_script(),
+    NMAP.packet_trace(),
+    NMAP.timingtemplate(NMAP.paranoid))
+```
+
+Execute a scan
+```
+scan = NMAP.run!(scanner)
+```
 """
 module NMAP
+
+    export Scan
+    export Scanner
 
     using Dates
     using EzXML
@@ -9,11 +37,11 @@ module NMAP
     using Lazy: Lazy, @forward
     using Printf
     using Revise
+    using StructTypes
     using XMLDict
 
     include("unmarshalling.jl")
-    using .Marshalling: Marshalling, Marsh, Leaf, fields, unmarshall
-    include("os.jl")
+    using .Marshalling: Marshalling, Marsh, Leaf, unmarshall
     include("xml.jl")
     include("scan.jl")
     include("options.jl")
